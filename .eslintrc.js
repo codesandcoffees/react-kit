466b486de65523705e3d18022b4849bbe92d1f7b
iamdevlinph <iamdevlinph@gmail.com>
2018-06-18 17:41:55 +0800

    Minor stuff

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,7 @@
 module.exports = {
   "extends": "airbnb",
   "rules": {
+    // react stuff
     "react/prefer-stateless-function": 0,
     "react/jsx-filename-extension": 0,
     "import/no-extraneous-dependencies": 0, // to allow the convenience package
