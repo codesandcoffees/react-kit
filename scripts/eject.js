fcd9dfb56c6fc7b50b20db088df9c8e191839247
iamdevlinph <iamdevlinph@gmail.com>
2018-07-26 23:58:38 +0800

    Add remove homepage from package.json task

--- scripts/eject.js
+++ scripts/eject.js
@@ -2,24 +2,45 @@ const fs = require('fs-extra');
 const COLORS = require('./colors');
 
 console.log(`${COLORS.FgYellow}Making this yours. Please wait a while.${COLORS.end}`);
+console.log()
+
+const warn = (string) => console.log(`${COLORS.BgRed}${COLORS.FgWhite}${string}${COLORS.end}`);
+const success = (string) => console.log(`${COLORS.FgGreen}${string}${COLORS.end}`);
 
 const removeFolder = (folder) => {
   fs.remove(folder, (err) => {
-    console.log(`${COLORS.BgRed}${COLORS.FgWhite}Removing ${folder} folder${COLORS.end}`);
+    warn(`Removing ${folder} folder`);
     if (err) throw err;
-    console.log(`${COLORS.FgGreen}${folder} folder deleted${COLORS.end}`);
+    success(`${folder} folder deleted`);
   });
 }
 
-const queue = (tasks) => {
+const removeHomepagePackageJson = () => {
+  fs.readFile('package.json', 'utf8', (err, data) => {
+    warn('Deleting homepage from package.json');
+    if (err) throw err;
+    const packageJson = JSON.parse(data);
+    delete packageJson.homepage;
+    fs.writeFile('package.json', JSON.stringify(packageJson, null, 2), (writeError) => {
+      if (writeError) throw writeError;
+      success('Updated package.json');
+    });
+  });
+}
+
+const run = (tasks) => {
   tasks.forEach((task, i) => {
+    if (!task.params) { // if no params are passed default an empty array
+      task.params = [];
+    }
     task.func(...task.params)
   });
 };
 
 const tasks = [
   { func: removeFolder, params: ['.git'] },
-  { func: removeFolder, params: ['docs'] }
+  { func: removeFolder, params: ['docs'] },
+  { func: removeHomepagePackageJson }
 ];
 
-queue(tasks);
+run(tasks);
