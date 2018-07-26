cc077f108403fc417b1f3d3e895105bdd85230a0
iamdevlinph <iamdevlinph@gmail.com>
2018-07-26 23:23:00 +0800

    Update console in pre-eject script

--- scripts/pre-eject.js
+++ scripts/pre-eject.js
@@ -2,6 +2,6 @@ const fs = require('fs');
 const COLORS = require('./colors');
 
 if (!fs.existsSync('node_modules/fs-extra')) {
-  console.log(`${COLORS.BgRed}${COLORS.FgWhite}Kindly run "npm install"${COLORS.end}`)
+  console.log(`${COLORS.BgRed}${COLORS.FgWhite}Kindly run "npm install fs-extra" first to proceed${COLORS.end}`)
   process.exit(1);
 }
