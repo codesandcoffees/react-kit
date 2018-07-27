132c15710139d2d4d02c4987e377b0dc353c4bac
iamdevlinph <iamdevlinph@gmail.com>
2018-07-27 12:53:15 +0800

    Changes to pre-eject script

--- scripts/pre-eject.js
+++ scripts/pre-eject.js
@@ -3,5 +3,6 @@ const COLORS = require('./colors');
 
 if (!fs.existsSync('node_modules/fs-extra')) {
   console.log(`${COLORS.FgRed}Kindly run "npm run mine:init" first to proceed${COLORS.end}`)
+  console.log();
   process.exit(1);
 }
