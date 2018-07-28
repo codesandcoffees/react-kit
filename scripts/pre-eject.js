5a57dcbc81e10eadec3d63461aa0f4d3eae3d30e
iamdevlinph <iamdevlinph@gmail.com>
2018-07-28 22:50:33 +0800

    Update npm script - mine

--- scripts/pre-eject.js
+++ scripts/pre-eject.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const COLORS = require('./colors');
 
 if (!fs.existsSync('node_modules/fs-extra')) {
-  console.log(`${COLORS.FgRed}Kindly run "npm run mine:init" first to proceed${COLORS.end}`)
+  console.log(`${COLORS.FgRed}Kindly run "npm i fs-extra" first to proceed${COLORS.end}`)
   console.log();
   process.exit(1);
 }
